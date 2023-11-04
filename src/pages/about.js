788e8a8b4b02277c2505b6bb3f55c1a6520bef62
john ro <johnro54@yahoo.com>
2023-11-03 19:09:20 -0700

    white space side responsive

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,19 +1,36 @@
+import React from 'react';
 import Image from 'react-bootstrap/Image';
-
 
 function About() {
   return (
-    <div style={{ position: 'relative', height: '100vh',width: '100%'}}>
-      <Image src= "https://images.unsplash.com/photo-1549769311-eb1f5f68d838?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2670&q=80" style={{ objectFit: 'cover', height: '100%', width: '100%' }} />
-      <div style={{ position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)', backgroundColor: 'rgba(0, 0, 0, 0.5)', padding: '20px', color: 'white', width: '90%', maxWidth: '900px'  }}>
-        <h1 style={{color:""}}>Wellness Coaching PNW</h1>
+    <div style={{ position: 'relative', height: '100vh', width: '100%', overflow: 'hidden' }}>
+      {/* Image Component */}
+      <Image
+        src="https://images.unsplash.com/photo-1549769311-eb1f5f68d838?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2670&q=80"
+        style={{ objectFit: 'cover', height: '100%', width: '100%', maxWidth: '100%' }}
+        alt="Wellness Coaching PNW"
+      />
+
+      {/* Overlay Text */}
+      <div style={{ 
+        position: 'absolute', 
+        top: '50%', 
+        left: '50%', 
+        transform: 'translate(-50%, -50%)', 
+        backgroundColor: 'rgba(0, 0, 0, 0.5)', 
+        padding: '20px', 
+        color: 'white', 
+        width: '90%', 
+        maxWidth: '900px'
+      }}>
+        <h1 style={{ color: '' }}>Wellness Coaching PNW</h1>
         <h3>Serving Residents of WA</h3>
         <p>
-        "If we did all the things we are capable of, we would literally astound ourselves." -Thomas Edison
+          "If we did all the things we are capable of, we would literally astound ourselves." - Thomas Edison
         </p>
       </div>
     </div>
   );
-} 
+}
 
 export default About;
