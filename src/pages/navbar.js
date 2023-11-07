252c391f360a08394a886a1c073b26087d87a196
john ro <johnro54@yahoo.com>
2023-11-07 15:43:45 -0800

    changed navbar mobile

--- src/pages/navbar.js
+++ src/pages/navbar.js
@@ -12,9 +12,9 @@ function NavbarBrand() {
         <Container >
         <Navbar.Brand href="#" style={{color:'#BDC4CB'}}>Wellness Coaching PNW</Navbar.Brand>
           <Navbar.Text >
-          <a href="#"><FaFacebook size={30} style={{ marginRight: '10px',color:'#BDC4CB' }}/></a>
-          <a href="mailto:wellnesscoachingpnw@gmail.com"><AiOutlineMail size={30} style={{ marginRight: '10px', color:'#BDC4CB' }}/></a>
-          <a href="#"><FaInstagram size={30} style={{color:'#BDC4CB'}}/></a>
+          <a href="#"><FaFacebook size={30} className= "mobile-nav" style={{ marginRight: '10px',color:'#BDC4CB' }}/></a>
+          <a href="mailto:wellnesscoachingpnw@gmail.com"><AiOutlineMail size={30} className= "mobile-nav" style={{ marginRight: '10px', color:'#BDC4CB' }}/></a>
+          <a href="#"><FaInstagram size={30} className= "mobile-nav" style={{color:'#BDC4CB'}}/></a>
           </Navbar.Text>
         </Container>
       </Navbar>
